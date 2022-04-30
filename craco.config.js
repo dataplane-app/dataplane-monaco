fb861e2e9831c1fb3d155fbe09f7025c47236253
saul-gush <admin@gush.ai>
2022-04-30 15:31:34 +0100

    monaco example

---
+++ craco.config.js
@@ -0,0 +1,8 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    webpack: {
+        plugins: [new MonacoWebpackPlugin()],
+    },
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import path from 'path';
 import './App.css';
+// import * as monaco from "monaco-editor";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import loader from "@monaco-editor/loader";
+// import path from 'path';
+
+// loader.config({ monaco });
+
+// loader.config({
+// paths: {
+//   vs: uriFromPath(
+//     path.join(__dirname, "../node_modules/monaco-editor/min/vs")
+//   )
+// }
+// });
+// loader.config({ paths: { vs: "../node_modules/monaco-editor/min/vs" } });
+
+// loader.init().then((monaco) => {
+//   const wrapper = document.getElementById("monaco");
+//   wrapper.style.height = "100vh";
+//   const properties = {
+//     value: "function hello() {\n\talert('Hello world!');\n}",
+//     language: "javascript"
+//   };
+
+//   monaco.editor.create(wrapper, properties);
+// });
+
+const uriFromPath = _path => {
+  let pathName = path.resolve(_path).replace(/\\/g, '/');
+
+  if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+    pathName = `/${pathName}`;
+  }
+  return encodeURI(`file://${pathName}`);
+};
+
+loader.config({
+    urls: {
+      monacoLoader: uriFromPath(
+        path.join(__dirname, '../node_modules/monaco-editor/min/vs/loader.js')
+      ),
+      monacoBase: uriFromPath(
+        path.join(__dirname, '../node_modules/monaco-editor/min/vs')
+      )
+    }
+  });
 
 function App() {
+  // const path = require('path');
+  const monaco2 = useMonaco();
+
+  useEffect(() => {
+    if (monaco2) {
+      console.log("here is the monaco instance:", monaco2);
+    }
+  }, [monaco2]);
+
+  // function ensureFirstBackSlash(str) {
+  //   return str.length > 0 && str.charAt(0) !== "/"
+  //       ? "/" + str
+  //       : str;
+  // }
+  
+  // function uriFromPath(_path) {
+  //   const pathName = path.resolve(_path).replace(/\\/g, "/");
+  //   return encodeURI("file://" + ensureFirstBackSlash(pathName));
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div id="monaco"></div> */}
+      <Editor
+                        height="90vh"
+      defaultValue="// some comment"
+      defaultLanguage="javascript"
+                    />
+
     </div>
   );
 }
