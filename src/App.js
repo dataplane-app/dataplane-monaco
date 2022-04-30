fddccc48a2dd229ea7a60f4b8f0568d6a993e343
saul-gush <admin@gush.ai>
2022-04-30 17:12:12 +0100

    working but "Loading..." still showing.

--- src/App.js
+++ src/App.js
@@ -27,26 +27,59 @@ import loader from "@monaco-editor/loader";
 
 //   monaco.editor.create(wrapper, properties);
 // });
+// var process = {platform: "other"}
 
-const uriFromPath = _path => {
-  let pathName = path.resolve(_path).replace(/\\/g, '/');
+// const uriFromPath = _path => {
+//   let pathName = path.resolve(_path).replace(/\\/g, '/');
 
-  if (pathName.length > 0 && pathName.charAt(0) !== '/') {
-    pathName = `/${pathName}`;
-  }
-  return encodeURI(`file://${pathName}`);
-};
+//   if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+//     pathName = `/${pathName}`;
+//   }
+//   return encodeURI(`file://${pathName}`);
+// };
 
-loader.config({
-    urls: {
-      monacoLoader: uriFromPath(
-        path.join(__dirname, '../node_modules/monaco-editor/min/vs/loader.js')
-      ),
-      monacoBase: uriFromPath(
-        path.join(__dirname, '../node_modules/monaco-editor/min/vs')
-      )
-    }
-  });
+// loader.config({
+//     urls: {
+//       monacoLoader: uriFromPath(
+//         path.join(__dirname, '../node_modules/monaco-editor/min/vs/loader.js')
+//       ),
+//       monacoBase: uriFromPath(
+//         path.join(__dirname, '../node_modules/monaco-editor/min/vs')
+//       )
+//     }
+//   });
+
+function ensureFirstBackSlash(str) {
+  return str.length > 0 && str.charAt(0) !== "/"
+      ? "/" + str
+      : str;
+}
+
+function uriFromPath(_path) {
+  const pathName = path.resolve(_path).replace(/\\/g, "/");
+  return encodeURI("file://" + ensureFirstBackSlash(pathName));
+}
+
+// loader.config({
+// paths: {
+//   vs: uriFromPath(
+//     path.join(__dirname, "../node_modules/monaco-editor/min/vs")
+//   )
+// }
+// });
+
+loader.config({ paths: { vs: "./node_modules/monaco-editor/min/vs" } });
+
+// loader.init().then((monaco) => {
+//   const wrapper = document.getElementById("root");
+//   wrapper.style.height = "100vh";
+//   const properties = {
+//     value: "function hello() {\n\talert('Hello world!');\n}",
+//     language: "javascript"
+//   };
+
+//   monaco.editor.create(wrapper, properties);
+// });
 
 function App() {
   // const path = require('path');
